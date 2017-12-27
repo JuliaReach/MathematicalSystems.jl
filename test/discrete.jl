c1b12594d33de78fc62e954281306cc40883faa8
mforets <marcelo.forets-irurtia@univ-grenoble-alpes.fr>
2017-12-27 16:44:24 -0300

    add discrete constrained tests

--- test/discrete.jl
+++ test/discrete.jl
@@ -1,7 +1,33 @@
 @testset "Discrete linear system" begin
     for sd in 1:3
-        s = DiscreteLinearSystem(zeros(sd, sd))
+        s = LinearDiscreteSystem(zeros(sd, sd))
         @test statedim(s) == sd
         @test inputdim(s) == 0
     end
+end
+
+@testset "Discrete linear control system" begin
+    for sd in 1:3
+        s = LinearControlDiscreteSystem(zeros(sd, sd), ones(sd, sd))
+        @test statedim(s) == sd
+        @test inputdim(s) == sd
+    end
+end
+
+@testset "Discrete constrained linear system" begin
+    A = [1. 1; 1 -1]
+    X = Line([1., -1], 0.) # line x = y
+    s = ConstrainedLinearDiscreteSystem(A, X)
+    @test statedim(s) == 2
+    @test inputdim(s) == 0
+end
+
+@testset "Discrete constrained linear control system" begin
+    A = [1. 1; 1 -1]
+    B = [0.5 1.5]'
+    X = Line([1., -1], 0.)
+    U = Hyperrectangle(low=[0.9, 0.9], high=[1.1, 1.2])
+    s = ConstrainedLinearControlDiscreteSystem(A, B, X, U)
+    @test statedim(s) == 2
+    @test inputdim(s) == 1
 end

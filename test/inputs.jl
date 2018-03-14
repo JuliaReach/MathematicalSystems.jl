df6867adbfed4e660d3d4ad7216124b8d15b0a20
schillic <schillic@informatik.uni-freiburg.de>
2018-03-14 18:06:59 +0100

    unit tests for nextinput

--- test/inputs.jl
+++ test/inputs.jl
@@ -1,5 +1,5 @@
 @testset "Constant input" begin
-     c = ConstantInput(1.0)
+    c = ConstantInput(1.0)
 
     @test eltype(c) == Float64
     @test Base.IteratorSize(typeof(c)) == Base.IsInfinite()
@@ -12,10 +12,17 @@
         i <= 10 || break
     end
     @test i == 11
+
+    @test  length(nextinput(c, 2)) == 2
+    i = 1
+    for val in nextinput(c, 2)
+        @test i <= 2 && val == 1.0
+        i += 1
+    end
  end
 
 @testset "Varying input" begin
-     v = VaryingInput([1.0, 2.0, 3.0])
+    v = VaryingInput([1.0, 2.0, 3.0])
 
     @test eltype(v) == Float64
     @test Base.IteratorSize(typeof(v)) == Base.HasLength()
@@ -23,4 +30,11 @@
 
     @test length(v) == 3
     @test collect(v) == [1.0, 2.0, 3.0]
+
+    @test  length(nextinput(v, 2)) == 2
+    i = 1
+    for val in nextinput(v, 2)
+        @test i <= 2 && val == v.U[i]
+        i += 1
+    end
  end

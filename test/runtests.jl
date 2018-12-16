b345730559e72a0c507e598155ebe1ff9c4fec22
mforets <mforets@gmail.com>
2018-12-16 17:32:25 -0300

    use VERSION check

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using MathematicalSystems, LazySets, TypedPolynomials
 using Compat, Compat.Test
-using SparseArrays
+
+@static if VERSION >= v"0.7-"
+    using SparseArrays
+end
 
 include("continuous.jl")
 include("discrete.jl")

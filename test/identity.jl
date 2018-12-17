2507c55229f085351cb30ab6d8a522ff05fa2cd3
mforets <mforets@gmail.com>
2018-12-17 14:24:58 -0300

    I -> UniformScaling

--- test/identity.jl
+++ test/identity.jl
@@ -3,15 +3,15 @@ using LazySets
 
 @testset "Creation of an identity multiple" begin
     for n in [1, 2, 1000]
-        In = IdentityMultiple(1.0*I, n)
+        In = IdentityMultiple(UniformScaling(1.0), n)
         @test size(In) == n
         @test In[1, 1] == 1.0 && In[1, 2] == 0.0
     end
 end
 
 @testset "Operations between identity multiples" begin
-    I2 = IdentityMultiple(1.0*I, 2)
-    I10 = IdentityMultiple(1.0*I, 10)
+    I2 = IdentityMultiple(UniformScaling(1.0), 2)
+    I10 = IdentityMultiple(UniformScaling(1.0), 10)
 
     @test (I2 + I2).M == UniformScaling(2.0)
     @test (10.0 * I2).M == UniformScaling(10.0)
@@ -26,7 +26,7 @@ end
     A, B = rand(4, 4), rand(4, 2)
     X = rand(Hyperrectangle, dim=4)
     U = rand(Ball2, dim=2)
-    I4 = IdentityMultiple(1.0*I, 4)
+    I4 = IdentityMultiple(UniformScaling(1.0), 4)
     s = ConstrainedLinearControlContinuousSystem(A, I4, X, B*U);
 
     @test statedim(s) == 4

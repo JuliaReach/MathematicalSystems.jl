569e4db932c5f6a82e456a0ee524564263b3028f
schillic <schillic@informatik.uni-freiburg.de>
2019-02-10 12:04:08 +0100

    add tests for constrained maps

--- test/maps.jl
+++ test/maps.jl
@@ -2,9 +2,18 @@
 import MathematicalSystems.LinearMap
 
 @testset "Identity map" begin
-    m = IdentityMap(5)
-    @test outputdim(m) == 5
+    m = IdentityMap(2)
+    @test outputdim(m) == 2
     @test islinear(m) && isaffine(m)
+    @test apply(m, ones(2)) == ones(2)
+end
+
+@testset "Constrained identity map" begin
+    X = Line([1., -1], 0.) # line x = y
+    m = ConstrainedIdentityMap(2, X)
+    @test outputdim(m) == 2
+    @test islinear(m) && isaffine(m)
+    @test stateset(m) == X
     @test apply(m, ones(2)) == ones(2)
 end
 
@@ -12,6 +21,22 @@ end
     A = [1. 1; 1 -1]
     m = LinearMap(A)
     @test outputdim(m) == 2
+    @test islinear(m) && isaffine(m)
+
+    # applying the linear map on a vector
+    @test apply(m, ones(2)) == [2.0, 0.0]
+
+    # applying the linear map on a set
+    X = BallInf(zeros(2), 1.0)
+    @test apply(m, X) == LazySets.LinearMap(A, X)
+end
+
+@testset "Constrained linear map" begin
+    A = [1. 1; 1 -1]
+    X = Line([1., -1], 0.) # line x = y
+    m = ConstrainedLinearMap(A, X)
+    @test outputdim(m) == 2
+    @test stateset(m) == X
     @test islinear(m) && isaffine(m)
 
     # applying the linear map on a vector
@@ -38,6 +63,24 @@ end
     @test !islinear(m) && isaffine(m)
 end
 
+@testset "Constrained affine map" begin
+    A = [1. 1; 1 -1]
+    b = [0.5, 0.5]
+    X = Line([1., -1], 0.) # line x = y
+    m = ConstrainedAffineMap(A, b, X)
+    @test outputdim(m) == 2
+    @test stateset(m) == X
+
+    # applying the affine map on a vector
+    @test apply(m, ones(2)) == [2.5, 0.5]
+
+    A = [1. 1.]
+    b = [0.5]
+    m = ConstrainedAffineMap(A, b, X)
+    @test outputdim(m) == 1
+    @test !islinear(m) && isaffine(m)
+end
+
 @testset "Linear control map" begin
     A = [1. 1; 1 -1]
     B = Matrix([0.5 1.5]')
@@ -54,7 +97,7 @@ end
 @testset "Constrained linear control map" begin
     A = [1. 1; 1 -1]
     B = Matrix([0.5 1.5]')
-    X = Line([1., -1], 0.)
+    X = Line([1., -1], 0.) # line x = y
     U = Interval(-1, 1)
     m = ConstrainedLinearControlMap(A, B, X, U)
     @test outputdim(m) == 2
@@ -63,6 +106,7 @@ end
     # applying the affine map on a vector
     x = ones(2) # (it is not checked that x ∈ X in this library)
     u = [1/2]   # same for u ∈ U
+    @test stateset(m) == X
     @test apply(m, x, u) == A*x + B*u
 end
 
@@ -88,6 +132,7 @@ end
     U = Interval(-1, 1)
     m = ConstrainedAffineControlMap(A, B, c, X, U)
     @test outputdim(m) == 2
+    @test stateset(m) == X
     @test !islinear(m) && isaffine(m)
 
     # applying the affine map on a vector

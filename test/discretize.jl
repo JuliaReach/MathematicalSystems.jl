38b16de9b5b372a18df4660a3b97777d884804d8
ueliwechsler <wueli@student.ethz.ch>
2019-12-22 12:54:59 +0100

    update tests

--- test/discretize.jl
+++ test/discretize.jl
@@ -1,20 +1,19 @@
 using LazySets
 using MathematicalSystems
 using Test
+using InteractiveUtils: subtypes
 
 @testset "Convert Continuous to Discrete Type" begin
     DTYPES = subtypes(AbstractDiscreteSystem)
     for dtype in DTYPES
         ctype =  eval.(Meta.parse.(replace(string(dtype), "Discrete" => "Continuous")))
-        @test _corresponding_type(dtype) == ctype
-        @test _corresponding_type(AbstractContinuousSystem, fieldnames(dtype)) == ctype
+        @test MathematicalSystems._complementary_type(dtype) == ctype
     end
 
     CTYPES = subtypes(AbstractContinuousSystem)
     for ctype in CTYPES
         dtype =  eval.(Meta.parse.(replace(string(ctype), "Continuous" => "Discrete")))
-        @test _corresponding_type(ctype) == dtype
-        @test _corresponding_type(AbstractDiscreteSystem, fieldnames(ctype)) == dtype
+        @test MathematicalSystems._complementary_type(ctype) == dtype
     end
 end
 
@@ -37,15 +36,13 @@ dict = Dict([:A => A, :B => B, :b => b, :c => c, :D => D,
              :X => X, :U => U, :W => W])
 dict_discretized = Dict([:A => A_d, :B => B_d, :b => b_d, :c => c_d, :D => D_d,
              :X => X, :U => U, :W => W])
-CFIELDS = fieldnames.(subtypes(AbstractContinuousSystem))
-filter!(x -> !(:f ∈ x), CFIELDS)
-filter!(x -> !(:p ∈ x), CFIELDS)
-filter!(x -> !(:E ∈ x), CFIELDS)
-filter!(x -> !(:statedim ∈ x), CFIELDS)
+# get affine ctypes
+CTYPES = filter(x -> (occursin("Linear", string(x)) || occursin("Affine", string(x))) &&
+                     !occursin("Algebraic", string(x)) , subtypes(AbstractContinuousSystem))
+DTYPES = MathematicalSystems._complementary_type.(CTYPES)
+CFIELDS = fieldnames.(CTYPES)
 CValues = [getindex.(Ref(dict), type) for type in CFIELDS]
 DValues = [getindex.(Ref(dict_discretized), type) for type in CFIELDS]
-CTYPES = _corresponding_type.(Ref(AbstractContinuousSystem), CFIELDS)
-DTYPES = _corresponding_type.(Ref(AbstractDiscreteSystem), CFIELDS)
 discretized_function = [discretize(CTYPES[i](CValues[i]...), ΔT) for i=1:length(CTYPES)]
 discretized_constructed = [DTYPES[i](DValues[i]...) for i=1:length(DTYPES)]
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,12 @@
 using MathematicalSystems, LazySets, TypedPolynomials
 using SparseArrays, LinearAlgebra
+using InteractiveUtils: subtypes
 using Test
 
 # fix namespace conflicts with LazySets
 using MathematicalSystems: LinearMap, AffineMap, ResetMap
 
 include("continuous.jl")
-include("discrete.jl")
 include("identity.jl")
 include("inputs.jl")
 include("maps.jl")
@@ -14,3 +14,5 @@ include("outputs.jl")
 include("macros.jl")
 include("successor.jl")
 include("utilities.jl")
+include("discretize.jl")
+

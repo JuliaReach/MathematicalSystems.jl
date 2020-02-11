89677822f2b7a52ad49091673e17ac54a765c56f
mforets <mforets@gmail.com>
2020-02-11 15:56:58 -0300

    add checks for repeated variable names in parse_system

--- src/macros.jl
+++ src/macros.jl
@@ -252,9 +252,12 @@ function parse_system(exprs)
     constraints = Vector{Expr}()
 
     # define defaults for state, noise and input symbol and default dimension
-    state_var = :x
-    input_var = :u
-    noise_var = :w
+    default_state_var = :x
+    default_input_var = :u
+    default_noise_var = :w
+    state_var = nothing
+    input_var = nothing
+    noise_var = nothing
     dimension = nothing
 
     # main loop to parse the subexpressions in exprs
@@ -313,8 +316,29 @@ function parse_system(exprs)
     nsets = length(constraints)
     nsets > 3 && throw(ArgumentError("cannot parse $nsets set constraints"))
 
-    return dynamic_equation, AT, constraints,
-           state_var, input_var, noise_var, dimension
+    # error handling for variable names
+    got_state_var = state_var != nothing
+    got_input_var = input_var != nothing
+    got_noise_var = noise_var != nothing
+    if (got_state_var && got_input_var) && (state_var == input_var)
+         throw(ArgumentError("state and input variables have the same name `$(state_var)`"))
+    elseif (got_state_var && got_noise_var) && (state_var == noise_var)
+         throw(ArgumentError("state and noise variables have the same name `$(state_var)`"))
+    elseif (got_input_var && got_noise_var) && (input_var == noise_var)
+         throw(ArgumentError("input and noise variables have the same name `$(input_var)`"))
+    end
+
+    # assign default values
+    if !got_state_var
+        state_var = default_state_var
+    end
+    if !got_input_var
+        input_var = default_input_var
+    end
+    if !got_noise_var
+        noise_var = default_noise_var
+    end
+    return dynamic_equation, AT, constraints, state_var, input_var, noise_var, dimension
 end
 
 # extract the field and value parameter from the dynamic equation `equation`

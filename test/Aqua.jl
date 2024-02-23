498a8c8d5fb1d16a5724a4f4c37ffdf0162636ea
schillic <git@christianschilling.net>
2024-02-23 21:50:59 +0100

    add Aqua to tests

---
+++ test/Aqua.jl
@@ -0,0 +1,24 @@
+using MathematicalSystems, Test
+import Aqua
+
+@testset "Aqua tests" begin
+    @static if VERSION < v"1.6"
+        unbound_args = true
+    else
+        # the unbound args should be resolved in the future
+        unbound_args = (broken=true,)
+    end
+
+    Aqua.test_all(MathematicalSystems;
+                  ambiguities=false, unbound_args=unbound_args,
+                  # the piracy of `sort` should be resolved in the future
+                  piracies=(broken=true,))
+
+    @static if VERSION < v"1.6"
+        # do not warn about ambiguities in dependencies
+        Aqua.test_ambiguities(MathematicalSystems)
+    else
+        # the ambiguities should be resolved in the future
+        Aqua.test_ambiguities(MathematicalSystems; broken=true)
+    end
+end

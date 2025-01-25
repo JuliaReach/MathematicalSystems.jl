fb3335925d6d63efcf20d3d053a1567f4d96af18
schillic <git@christianschilling.net>
2025-01-25 18:06:24 +0100

    use DocumenterCitations for bibliography

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,24 @@
-using Documenter, MathematicalSystems
+using Documenter, MathematicalSystems, DocumenterCitations
 import LazySets
 
 DocMeta.setdocmeta!(MathematicalSystems, :DocTestSetup,
                     :(using MathematicalSystems); recursive=true)
 
+bib = CitationBibliography(joinpath(@__DIR__, "src", "refs.bib"); style=:alpha)
+
 makedocs(; sitename="MathematicalSystems.jl",
          modules=[MathematicalSystems],
          format=Documenter.HTML(; prettyurls=get(ENV, "CI", nothing) == "true",
-                                assets=["assets/aligned.css"], size_threshold_warn=150 * 2^10),
+                                assets=["assets/aligned.css", "assets/citations.css"],
+                                size_threshold_warn=150 * 2^10),
          pagesonly=true,
+         plugins=[bib],
          pages=["Home" => "index.md",
                 "Manual" => Any["Overview of System Types" => "man/systems.md"],
                 "Library" => Any["Types"     => "lib/types.md",
                                  "Methods"   => "lib/methods.md",
                                  "Internals" => "lib/internals.md"],
+                "Bibliography" => "bibliography.md",
                 "About" => "about.md"])
 
 deploydocs(; repo="github.com/JuliaReach/MathematicalSystems.jl.git",
